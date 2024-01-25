a79c2e58de0e86a5c1f21651079688906aacaef6
mashriram <mashriram@gmail.com>
2024-01-25 15:13:36 +0530

    fix:ui

--- TaskManager/src/main/java/com/project/TaskManager/TaskController.java
+++ TaskManager/src/main/java/com/project/TaskManager/TaskController.java
@@ -1,15 +1,18 @@
 package com.project.TaskManager;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
 @RequestMapping("tasks")
 public class TaskController {
 
@@ -17,39 +20,39 @@ public class TaskController {
     private TaskServices taskServices;
 
     @GetMapping("/")
-//    @ResponseBody
-    public String getAllTasks(Model model){
+    // @ResponseBody
+    public String getAllTasks(Model model) {
         List<Task> taskList = taskServices.getAllTasks();
         model.addAttribute("tasks", taskList);
-        return "list" ;
+        return "list";
     }
+
     @GetMapping("/{id}")
-    public String getTaskById(@PathVariable Long id, Model model){
+    public String getTaskById(@PathVariable Integer id, Model model) {
         Optional<Task> taskOptional = taskServices.getTaskById(id);
-        if(taskOptional.isPresent()){
+        if (taskOptional.isPresent()) {
             Task task = taskOptional.get();
-            model.addAttribute("tasks",task);
+            model.addAttribute("tasks", task);
             return "details";
-        }
-        else{
+        } else {
             return "redirect:/tasks";
         }
     }
+
     @GetMapping("/save")
-    public String saveTask(@ModelAttribute Task task,RedirectAttributes redirectAttributes){
+    public String saveTask(@ModelAttribute Task task, RedirectAttributes redirectAttributes) {
         Task savedTask = taskServices.saveTask(task);
-        redirectAttributes.addFlashAttribute("success","Task has been saved successfully");
-        return "redirect:/tasks"+savedTask.getId();
+        redirectAttributes.addFlashAttribute("success", "Task has been saved successfully");
+        return "redirect:/tasks" + savedTask.getId();
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteTaskById(@PathVariable Long id,RedirectAttributes redirectAttributes){
-        try{
+    public String deleteTaskById(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        try {
             taskServices.deleteTaskById(id);
-            redirectAttributes.addFlashAttribute("DeleteTask","Task Deleted Succesfully");
-        }
-        catch(Exception e){
-            redirectAttributes.addFlashAttribute("Error","Unable to Delete "+e.getMessage());
+            redirectAttributes.addFlashAttribute("DeleteTask", "Task Deleted Succesfully");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("Error", "Unable to Delete " + e.getMessage());
         }
         return "redirect:/tasks";
     }
--- TaskManager/src/main/java/com/project/TaskManager/TaskServices.java
+++ TaskManager/src/main/java/com/project/TaskManager/TaskServices.java
@@ -1,36 +1,37 @@
 package com.project.TaskManager;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class TaskServices {
     @Autowired
     private TaskRepository taskRepository;
-    public List<Task> getAllTasks(){
+
+    public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
-    public Optional<Task> getTaskById(Long id){
+    public Optional<Task> getTaskById(Integer id) {
         return Optional.of(taskRepository.getReferenceById(id));
     }
 
-    public Task saveTask(Task task){
+    public Task saveTask(Task task) {
         return taskRepository.save(task);
     }
 
-    public void deleteTaskById(Long id){
+    public void deleteTaskById(Integer id) {
         taskRepository.deleteById(id);
     }
 
-//    public void deleteAllTasks(){
-//        taskRepository.deleteAll();
-//    }
-//
-//    public Long numberOfTasks(){
-//        return taskRepository.count();
-//    }
+    // public void deleteAllTasks(){
+    // taskRepository.deleteAll();
+    // }
+    //
+    // public Long numberOfTasks(){
+    // return taskRepository.count();
+    // }
 }
